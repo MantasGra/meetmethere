1d74f2148ee862f782ea1f7006d28e8d34e9e2fe
Mantas <mantelisas@gmail.com>
2021-03-28 23:02:59 +0300

    Add eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,9 @@ import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
+  it('renders initial app message link', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const linkElement = getByText(/This will be the MeetMeThere app\./i);
     expect(document.body.contains(linkElement));
   });
 });
